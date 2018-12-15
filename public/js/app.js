9ef707c6688b2fbfa18ca4846b45887ce1e5b3dc
doulos tou Christou <jeremyhancock32@gmail.com>
2018-12-15 12:48:39 -0500

    everything works, but some styles need to be fixed

--- public/js/app.js
+++ public/js/app.js
@@ -1,74 +1,84 @@
 // Grab the articles as a json
-$.getJSON("/articles", function(data) {
-    // For each one
-    for (var i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
-      $("#articles").append("<p data-id='" + data[i]._id + "'> <img src=" + data[i].image + "><br>" + data[i].title + "<br />" + data[i].link + "</p>");
-      // $("#articles").append("<p data-id='" + data[i]._id + "'><h4>" + data[i].title + "</h4><a href='" + data[i].link + "' target='_blank'> <img src='" + data[i].image + "'></a><br /><br /> </p>");
+$.getJSON("/api/articles", function (data) {
+  // For each one
+  for (var i = 0; i < data.length; i++) {
+    // Display the apropos information on the page
+    $("#articles").prepend("<a href='" + data[i].link + "' target='_blank'> <img src='" + data[i].image + "'></a></p>");
+    $("#articles").prepend("<p data-id='" + data[i]._id + "'>" + data[i].title + "</p>");
+  }
+});
+
+// Whenever someone clicks a p tag
+$(document).on("click", "p", function () {
+  // Empty the notes from the note section
+  $("#comments").empty();
+  // Save the id from the p tag
+  var thisId = $(this).attr("data-id");
+
+  // Now make an ajax call for the Article
+  $.ajax({
+    method: "GET",
+    url: "/api/articles/" + thisId
+  })
+    // With that done, add the note information to the page
+    .then(function (data) {
+      // The title of the article
+      $("#comments").append("<p>" + data.title + "</p>");
+      // An input to enter a author
+      $("#comments").append("<input id='authorinput' name='author' placeholder='Your name'>");
+      // A textarea to add a new note body
+      $("#comments").append("<textarea id='bodyinput' name='body' placeholder='Your comment'></textarea>");
+      // A button to submit a new note, with the id of the article saved to it
+      $("#comments").append("<button data-id='" + data._id + "' id='saveComment'>Save Comment</button>");
+    })
+  // If there's a comment attached to the article
+  $.ajax({
+    method: "GET",
+    url: "/api/comments/" + thisId
+  })
+    .then(function (data) {
+      console.log(data);
+for (i=0; i<data.length; i++){
+  var parseTimestamp = data[i].timestamp;
+  var commentTime = 
+        // Using the timestamp of the comment ...
+      $("#pastComments").append("<p> At " + data[i].timestamp + " " + data[i].author + " said ... <p>")
+      // Place the body of the note in the body textarea
+      $("#pastComments").append("<p>" + data[i].body + "</p>");
+      $("#pastComments").append("<p> -----------------------------------------</p>")
     }
   });
-  
-  
-  // Whenever someone clicks a p tag
-  $(document).on("click", "p", function() {
-    // Empty the notes from the note section
-    $("#comments").empty();
-    // Save the id from the p tag
-    var thisId = $(this).attr("data-id");
-  
-    // Now make an ajax call for the Article
-    $.ajax({
-      method: "GET",
-      url: "/articles/" + thisId
-    })
-      // With that done, add the note information to the page
-      .then(function(data) {
-        console.log(data);
-        // The title of the article
-        $("#comments").append("<h2>" + data.title + "</h2>");
-        // An input to enter a new title
-        $("#comments").append("<input id='titleinput' name='title' >");
-        // A textarea to add a new note body
-        $("#comments").append("<textarea id='bodyinput' name='body'></textarea>");
-        // A button to submit a new note, with the id of the article saved to it
-        $("#comments").append("<button data-id='" + data._id + "' id='saveComment'>Save Comment</button>");
-  
-        // If there's a note in the article
-        if (data.comment) {
-          // Place the title of the note in the title input
-          $("#titleinput").val(data.comment.title);
-          // Place the body of the note in the body textarea
-          $("#bodyinput").val(data.comment.body);
-        }
-      });
   });
-  
-  // When you click the saveComment button
-  $(document).on("click", "#saveComment", function() {
-    // Grab the id associated with the article from the submit button
-    var thisId = $(this).attr("data-id");
-  
-    // Run a POST request to change the note, using what's entered in the inputs
-    $.ajax({
-      method: "POST",
-      url: "/articles/" + thisId,
-      data: {
-        // Value taken from title input
-        title: $("#titleinput").val(),
-        // Value taken from note textarea
-        body: $("#bodyinput").val()
-      }
-    })
-      // With that done
-      .then(function(data) {
-        // Log the response
-        console.log(data);
-        // Empty the notes section
-        $("#comments").empty();
-      });
-  
-    // Also, remove the values entered in the input and textarea for note entry
-    $("#titleinput").val("");
-    $("#bodyinput").val("");
-  });
-  
+
+// When you click the saveComment button
+$(document).on("click", "#saveComment", function () {
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
+  // Run a POST request to create the comment, using what's entered in the inputs
+  $.ajax({
+    method: "POST",
+    url: "/api/comments/" + thisId,
+    data: {
+      // Value taken from data-id
+      articleId: thisId,
+      // Value taken from authorinput
+      author: $("#authorinput").val(),
+      // Value taken from note textarea
+      body: $("#bodyinput").val(),
+      // Attach the current time
+      timestamp: Date.now()
+    }
+  })
+    // With that done
+    .then(function (data) {
+      console.log(data);
+      // Empty the notes section
+      $("#comments").empty();
+      $("#pastComments").empty();
+    });
+
+  // Also, remove the values entered in the input and textarea for note entry
+  $("#authorinput").val("");
+  $("#bodyinput").val("");
+
+});
--- server.js
+++ server.js
@@ -33,7 +33,7 @@ mongoose.connect(MONGODB_URI);
 // Routes
 
 // A GET route for scraping the echoJS website
-app.get("/scrape", function(req, res) {
+app.get("/api/scrape", function(req, res) {
     // First, we grab the body of the html with axios
     axios.get("https://nerdist.com/categories/").then(function(response) {
       // Then, we load that into cheerio and save it to $ for a shorthand selector
@@ -70,7 +70,7 @@ app.get("/scrape", function(req, res) {
   });
   
   // Route for getting all Articles from the db
-  app.get("/articles", function(req, res) {
+  app.get("/api/articles", function(req, res) {
     // Grab every document in the Articles collection
     db.Article.find({})
       .then(function(dbArticle) {
@@ -82,13 +82,26 @@ app.get("/scrape", function(req, res) {
         res.json(err);
       });
   });
+
+    // Route for getting all Articles from the db
+    app.get("/api/comments", function(req, res) {
+      // Grab every document in the Articles collection
+      db.Comment.find({})
+        .then(function(dbComment) {
+          // If we were able to successfully find Articles, send them back to the client
+          res.json(dbComment);
+        })
+        .catch(function(err) {
+          // If an error occurred, send it to the client
+          res.json(err);
+        });
+    });
   
   // Route for grabbing a specific Article by id, populate it with it's note
-  app.get("/articles/:id", function(req, res) {
+  app.get("/api/articles/:id", function(req, res) {
     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
     db.Article.findOne({ _id: req.params.id })
       // ..and populate all of the notes associated with it
-      .populate("comment")
       .then(function(dbArticle) {
         // If we were able to successfully find an Article with the given id, send it back to the client
         res.json(dbArticle);
@@ -98,25 +111,40 @@ app.get("/scrape", function(req, res) {
         res.json(err);
       });
   });
+    // Route for grabbing a specific Article by id, populate it with it's note
+    app.get("/api/comments/:id", function(req, res) {
+      // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+      db.Comment.find({ articleId: req.params.id })
+        // ..and populate all of the notes associated with it
+        .then(function(dbComment) {
+          // If we were able to successfully find an Article with the given id, send it back to the client
+          res.json(dbComment);
+        })
+        .catch(function(err) {
+          // If an error occurred, send it to the client
+          res.json(err);
+        });
+    });
   
-  // Route for saving/updating an Article's associated Note
-  app.post("/articles/:id", function(req, res) {
-    // Create a new note and pass the req.body to the entry
+  // Route for saving/updating an Article's associated Comment
+  app.post("/api/comments/:id", function(req, res) {
+    // Create a new comment and pass the req.body to the entry
     db.Comment.create(req.body)
       .then(function(dbComment) {
-        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+        // If a Comment was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Comment
         // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
         // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-        return db.Article.findOneAndUpdate({ _id: req.params.id }, { comment: dbComment._id }, { new: true });
-      })
+        return db.Article.findOneAndUpdate({ "_id": req.params.id }, { commentId: dbComment._id }, { new: true })
       .then(function(dbArticle) {
         // If we were able to successfully update an Article, send it back to the client
+        console.log(dbArticle);
         res.json(dbArticle);
       })
       .catch(function(err) {
         // If an error occurred, send it to the client
         res.json(err);
       });
+    });
   });
   
   // Start the server
